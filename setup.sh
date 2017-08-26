fb8c463583854ecb20e4a5c8623516e5f86d0540
girishraghunath <girish.raghunath@gmail.com>
2017-08-26 11:06:58 +1000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+npm i -g npm
+npm install -g elm
+npm install -g elm-format
+npm install -g elm-test
+npm install -g node-sass
+apm install elm
+apm install language-elm
+apm install elm-format
+apm install linter-elm-make
+apm install elmjutsu
